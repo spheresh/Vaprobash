4a726a318fef94ceeb2778363f484b28861e46e9
Yuriy Boychenko <yuboychenko@gmail.com>
2015-01-18 03:36:31 +0000

    mpm_event module was disabled

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -48,9 +48,9 @@ sudo usermod -a -G www-data vagrant
 # Apache Config
 # On separate lines since some may cause an error
 # if not installed
-sudo a2dismod mpm_prefork mpm_worker
-sudo a2dismod php5
-sudo a2enmod rewrite actions ssl
+sudo a2dismod mpm_prefork mpm_event
+sudo a2dismod php5 
+sudo a2enmod mpm_worker rewrite actions ssl
 curl --silent -L $github_url/helpers/vhost.sh > vhost
 sudo chmod guo+x vhost
 sudo mv vhost /usr/local/bin
